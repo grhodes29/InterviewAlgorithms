b18c50ccd4830eae434b2e00c2e6240d8966079f
grhodes29 <grhodes29@gmail.com>
2019-02-17 08:33:47 -0600

    Google interview question - https://www.youtube.com/watch?v=GJdiM-muYqc&t=29s

---
+++ GoogleInterviewStringQuestionOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleInterviewStringQuestionOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Give a string of any length, return the first repeating char
+            // EXAMPLES:    ADKLSSASLKDLSSAE  --> S
+            //              BABSDFLKJA --> B
+            //              ASKDL       --> NULL 
+
+        }
+    }
+}
